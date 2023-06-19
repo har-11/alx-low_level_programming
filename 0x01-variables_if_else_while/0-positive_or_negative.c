a8f9ebdc873d59c0362e825e9cfb9ca2aabfdb3e
Hamza <enovahamza@gmail.com>
2023-06-19 16:48:58 -0700

    ifwhile

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void) {
     int sign = (n < 0) ? -1 : 1;
 
     if (lastDigit > 5) {
-        printf("%d and is greater than 5\n", sign * lastDigit);
+        printf("%d and is greater than 5\n", lastDigit);
     } else if (lastDigit == 0) {
         printf("%d and is 0\n", lastDigit);
     } else {
-        printf("%d and is less than 6 and not 0\n", sign * lastDigit);
+        printf("%d and is less than 6 and not 0\n", lastDigit);
     }
 
     return 0;
