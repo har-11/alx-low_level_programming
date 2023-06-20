afd1fef347b3c2af7e20155bedf3221987b75beb
Hamza <enovahamza@gmail.com>
2023-06-19 17:05:55 -0700

    ifwhile

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
  * *
  * Return: Always = (0)
  */
-int main(void) {
-	char c;
-	
+	int main(void) 
+{
+	char c;	
 	for (c = 'a'; c <= 'z'; c++)
 	{
-    	putchar(c);
+	putchar(c);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
