3db8d80c1b4f32845087a701ca1c2d422108c678
Hamza <enovahamza@gmail.com>
2023-06-19 17:23:47 -0700

    ifwhile

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stduio.h>
+
+/** 
+ * main - Entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char c;
+	for (c ='a'; c <='z'; c !='e'; c !='q' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
