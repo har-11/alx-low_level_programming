ea70966cfbaac82f175da34619523cf064260c89
Hamza <enovahamza@gmail.com>
2023-06-19 17:41:38 -0700

    ifwhile

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char(c);
+
+	for (c ='0' ; c <= '9'; c++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return: (0)
+}
