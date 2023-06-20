30afc2c3ffe912d2852a05c43e216b79bbe692de
Hamza <enovahamza@gmail.com>
2023-06-19 17:13:50 -0700

    ifwhile

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - The Entry point
+ * *
+ * Return: Always = (0)
+ */
+int main(void)
+{
+char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	 for (c = 'A'; c <= 'Z'; c++)
+        {
+        putchar(c);
+        }
+        putchar('\n')
+	return (0);
+}
