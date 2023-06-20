3e6690358a56f6aae9ef086a724203becc40af0b
Hamza <enovahamza@gmail.com>
2023-06-19 17:28:16 -0700

    ifwhile

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,12 @@
 int main(void)
 {
 	char c;
-	for (c ='a'; c <='z'; c !='e'; c !='q' ; c++)
+	for (c ='a'; c <='z'; c++)
+	{
+	if (c !='e' && c !='q')
 	{
 		putchar(c);
+	}
 	}
 	putchar('\n');
 	return (0);
