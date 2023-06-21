d1ed5fe1d1a286d8bb5fe7fdfe3492dda97d865d
Hamza <enovahamza@gmail.com>
2023-06-20 17:44:36 -0700

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	char c;
+char c;
 
-	for(c = "a" ; c <= "z" ; c++)
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
-		putchar("c");
+	putchar(c);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
