46d83de4bce707c6a1577d48f54c26e2c25c2fbd
Hamza <enovahamza@gmail.com>
2023-06-20 17:37:23 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char(c)
+
+	int(c = "a" ; c <= "z" ; c++)
+	{
+		putchar("c");
+	}
+	putchar("\n");
+	return (0);
+}
