103ff18f4201bfd09cfef7802c5ec485aada8a1e
Hamza <enovahamza@gmail.com>
2023-06-26 17:17:58 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,10 +7,14 @@
  */
 
 void reset_to_98(int *n);
+{
+	*n = 98;
+}
 
 int main(void)
 {
 	int number = 5;
+
 	printf("Before reset: %d\n", number);
 	reset_to_98(&number);
 	printf("After reset: %d\n", number);
