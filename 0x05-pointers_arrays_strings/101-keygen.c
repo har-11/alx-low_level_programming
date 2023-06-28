24cdf3db1eb0f2c2d08849136023476c0199a618
Hamza <enovahamza@gmail.com>
2023-06-27 19:57:45 -0700

    pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -20,11 +20,30 @@ void generate_password(char *password, int length)
     password[length] = '\0';
 }
 
+int is_valid_password(char *password)
+{
+    // Modify this function based on the requirements of 101-crackme
+    // Check if the generated password meets the desired criteria
+    // Return 1 if it's valid, 0 otherwise
+    // Example:
+    // if (strlen(password) >= 8 && has_uppercase(password) && has_digit(password))
+    //     return 1;
+    // else
+    //     return 0;
+    // Replace has_uppercase and has_digit with appropriate checks
+
+    // For now, assume any password is valid
+    return 1;
+}
+
 int main(void)
 {
     char password[PASSWORD_LENGTH + 1];
 
-    generate_password(password, PASSWORD_LENGTH);
+    do {
+        generate_password(password, PASSWORD_LENGTH);
+    } while (!is_valid_password(password));
+
     printf("Generated password: %s\n", password);
 
     return 0;
