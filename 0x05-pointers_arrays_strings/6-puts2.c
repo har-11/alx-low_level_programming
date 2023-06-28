cf8a6c72ecba587df951bb170f38225b9773c455
Hamza <enovahamza@gmail.com>
2023-06-27 19:43:05 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,9 +14,9 @@ void puts2(char *str)
 
     while (str[i] != '\0')
     {
-        putchar(str[i]);
+	            printf("%c", str[i]);
         i += 2;
     }
 
-    putchar('\n');
+    printf("\n");
 }
