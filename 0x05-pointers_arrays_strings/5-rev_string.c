074a8f6425ee2d4573f1565d4bc21c4548178fdf
Hamza <enovahamza@gmail.com>
2023-06-27 19:34:35 -0700

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,29 +8,29 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    int start = 0;
-    int end = 0;
-    char temp;
+	int length = 0;
+	int start = 0;
+	int end = 0;
+	char temp;
 
-    /* Calculate the length of the string*/
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	/* Calculate the length of the string*/
+	while (s[length] != '\0')
+{
+	length++;
+}
 
-    /* Reverse the string*/
-    start = 0;
-    end = length - 1;
-    while (start < end)
-    {
-        /* Swap characters at start and end positions*/
-        temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+	/* Reverse the string*/
+	start = 0;	
+	end = length - 1;	
+	while (start < end)
+{
+	/* Swap characters at start and end positions*/
+	temp = s[start];
+	s[start] = s[end];
+	s[end] = temp;
 
-        /* Move start and end positions towards the center*/
-        start++;
-        end--;
-    }
+	/* Move start and end positions towards the center*/
+	start++;
+	end--;
+}
 }
