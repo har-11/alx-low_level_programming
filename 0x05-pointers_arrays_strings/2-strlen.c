c70431fd24b56212d75f2eeb7305d17a67948ac0
Hamza <enovahamza@gmail.com>
2023-06-27 18:15:50 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(char *s) {
+    int length = 0;
+
+    while (*s != '\0') {
+        length++;
+        s++;
+    }
+
+    return length;
+}
+
+int main() {
+    char *str = "Hello, World!";
+
+    int length = _strlen(str);
+
+    printf("Length of the string: %d\n", length);
+
+    return 0;
+}
