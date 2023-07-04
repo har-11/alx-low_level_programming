55f2a2da784e8bac81efcca246b43f7aff37e225
Hamza <enovahamza@gmail.com>
2023-07-04 11:35:00 -0700

    more_functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numberss 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+			}
+		}
+	}
+	putchar('\n');
+}
