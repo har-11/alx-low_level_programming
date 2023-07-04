da159cfe783082907a6319c7ee5b2a45d33e1cc9
Hamza <enovahamza@gmail.com>
2023-07-04 14:01:34 -0700

    more_functions

---
+++ 0x04-more_functions_nested_loops/101-print-numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+			putchar((n % 10) + '0');
+}
