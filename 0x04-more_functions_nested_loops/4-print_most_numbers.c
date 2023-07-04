e19ff85e14b9e59a04c947e88dc54752fc8aac3f
Hamza <enovahamza@gmail.com>
2023-07-04 11:14:17 -0700

    more_functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - as its say
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (i != 2 || i != 4)
+		{
+			putchat(i + '0');
+		}
+	}
+	putchar('\n');
+}
